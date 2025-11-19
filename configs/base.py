3b0f5323e4f6da895a6c282d9013e3ea8b2a8910
Jeff Ma <jeff.ma713@gmail.com>
2025-11-19 15:14:56 -0800

    modify config file for testing

--- configs/base.py
+++ configs/base.py
@@ -4,10 +4,18 @@ maximum_trials = 3
 device = 'cuda:0'
 make_video = True
 
+# benchmark_setting = dict(
+#     games=['tictactoe', 'gomoku', 'minesweeper', 'reversi', 'sudoku', 'chess'],
+#     sample_size=2000,
+#     e2e_round=100,
+#     offline_task=['perceive', 'qa', 'rule'],
+#     benchmark_path='benchmark'
+# )
+# duplicate, preserve old for references
 benchmark_setting = dict(
-    games=['tictactoe', 'gomoku', 'minesweeper', 'reversi', 'sudoku', 'chess'],
-    sample_size=2000,
+    games=['tictactoe'],
+    sample_size=100,
     e2e_round=100,
-    offline_task=['perceive', 'qa', 'rule'],
+    offline_task=['perceive'],
     benchmark_path='benchmark'
 )
--- configs/recipe/base.py
+++ configs/recipe/base.py
@@ -1,4 +1,10 @@
+# name = 'standard'
+# save_path = 'experiments'
+# tasks = ['perceive', 'qa', 'rule', 'e2e']
+# games = ['tictactoe', 'reversi', 'gomoku', 'minesweeper', 'sudoku', 'chess']
+
+# duplicate, preserve old for references
 name = 'standard'
 save_path = 'experiments'
-tasks = ['perceive', 'qa', 'rule', 'e2e']
-games = ['tictactoe', 'reversi', 'gomoku', 'minesweeper', 'sudoku', 'chess']
+tasks = ['perceive']
+games = ['tictactoe']
