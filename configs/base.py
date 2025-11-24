bff5589faf5b5fe0412e4da53e4b40af227ff535
uttuliv <yeyuxuanx@gmail.com>
2025-11-23 18:02:28 -0800

    change base.py status

--- configs/base.py
+++ configs/base.py
@@ -4,10 +4,14 @@ maximum_trials = 3
 device = 'cuda:0'
 make_video = True
 
+
 benchmark_setting = dict(
-    games=['tictactoe', 'gomoku', 'minesweeper', 'reversi', 'sudoku', 'chess'],
-    sample_size=2000,
-    e2e_round=100,
-    offline_task=['perceive', 'qa', 'rule'],
-    benchmark_path='benchmark'
+   # games=['tictactoe', 'gomoku', 'minesweeper', 'reversi', 'sudoku', 'chess'],
+   games=['tictactoe'],
+   sample_size=100,
+   e2e_round=100,
+   # offline_task=['perceive', 'qa', 'rule'],
+   offline_task=['perceive'],
+   benchmark_path='benchmark'
 )
+
--- configs/recipe/base.py
+++ configs/recipe/base.py
@@ -1,4 +1,6 @@
 name = 'standard'
 save_path = 'experiments'
-tasks = ['perceive', 'qa', 'rule', 'e2e']
-games = ['tictactoe', 'reversi', 'gomoku', 'minesweeper', 'sudoku', 'chess']
+# tasks = ['perceive', 'qa', 'rule', 'e2e']
+tasks = ['perceive']
+# games = ['tictactoe', 'reversi', 'gomoku', 'minesweeper', 'sudoku', 'chess']
+games = ['tictactoe']
