40a8b9d2893a471e0c4b52f6cb54f3471bef675a
Shixian (Emily) Chen <70627038+emilime162@users.noreply.github.com>
2025-12-07 16:41:54 -0800

    Update max_new_token

--- configs/agents/custom/MiniCPM-V-2_6.py
+++ configs/agents/custom/MiniCPM-V-2_6.py
@@ -7,5 +7,6 @@ lmm_agent = dict(
     chat_template=None,
     model='openbmb/MiniCPM-V-2_6',
     backend_config=TurbomindEngineConfig(session_len=8192),
-    general_config=GenerationConfig(max_new_tokens=4096, top_p=0.8),
+    general_config=GenerationConfig(max_new_tokens=1024, top_p=0.8),
 )
+
